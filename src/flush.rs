892d0d9f5cd23836977bcee74ef24b31ed06c6d8
teltam <telt.yellow.tail@gmail.com>
2024-10-28 00:50:07 -0700

    WIP sst without index

--- src/flush.rs
+++ src/flush.rs
@@ -1,11 +1,81 @@
+use std::fmt;
 use std::fs::File;
 use std::io::Write;
-use crate::mem_table::MemTable;
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+use crate::mem_table::{MemTable, MemTableEntry};
+
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+struct Index {
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+struct DataBlock {
+    // TODO Mimics MemTableEntry because we aren't doing compression or delta-decoding just yet.
+    block: Vec<MemTableEntry>,
+    hash: String,
+}
+
+impl fmt::Display for DataBlock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, " block {:?} hash {}", self.block, self.hash)
+    }
+}
+
+impl DataBlock {
+    pub fn new(block_size: usize) -> DataBlock {
+        DataBlock {
+            block: Vec::with_capacity(block_size),
+            hash: "None".to_string(),
+        }
+    }
+
+    pub fn hash(&self) -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(self.to_string());
+        let digest = format!("{:X}", hasher.finalize());
+        digest
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+struct StaticSortedTable {
+    // aka Sorted String Table
+    index: Index,
+
+    data_blocks: Vec<DataBlock>,
+    block_size: usize,
+}
 
 // TODO Flush worker file, could be a new thread?
 // TODO what happens if the json, file create, file write fails?
 pub fn flush(mem_table: &MemTable) -> std::io::Result<MemTable> {
-    let flush_data = serde_json::to_string(mem_table)?;
+
+    let block_size = 4;
+
+    let mut data_blocks: Vec<DataBlock> = vec!();
+    for (i, entry) in mem_table.data.iter().enumerate() {
+        if i % block_size == 0 {
+            data_blocks.push(DataBlock::new(block_size))
+        }
+
+        let db_len = data_blocks.len();
+
+        data_blocks[db_len-1].block.push(entry.clone())
+    }
+
+    for data_block in &mut data_blocks {
+        data_block.hash = data_block.hash();
+    }
+
+    let sst = StaticSortedTable {
+        index: Index {}, // TODO
+        data_blocks,
+        block_size,
+    };
+
+    let flush_data = serde_json::to_string(&sst)?;
     let _ = std::fs::create_dir_all("/Users/stangirala/.ferrum_store/")?;
     let mut file = File::create("/Users/stangirala/.ferrum_store/sst_0")?;
 
@@ -14,10 +84,10 @@ pub fn flush(mem_table: &MemTable) -> std::io::Result<MemTable> {
     Ok(MemTable::new())
 }
 
-fn _load_from_file() -> std::io::Result<MemTable> {
+fn _load_from_file() -> std::io::Result<StaticSortedTable> {
     let contents = std::fs::read_to_string("/Users/stangirala/.ferrum_store/sst_0")?;
-    let mem_table = serde_json::from_str(&contents)?;
-    Ok(mem_table)
+    let sst: StaticSortedTable = serde_json::from_str(&contents)?;
+    Ok(sst)
 }
 
 
@@ -36,7 +106,7 @@ mod tests {
             value: 0,
         });
 
-        let new_mem_table = match flush(&mem_table){
+        let new_mem_table = match flush(&mem_table) {
             Ok(m) => { m },
             Err(e) => { panic!("{:?}", e); }
         };
@@ -44,10 +114,31 @@ mod tests {
         assert_ne!(addr_of!(mem_table), addr_of!(new_mem_table));
         assert_ne!(addr_of!(mem_table.data), addr_of!(new_mem_table.data));
         assert_ne!(addr_of!(mem_table.wal), addr_of!(new_mem_table.wal));
+    }
 
-        let from_file_mem_table = _load_from_file().unwrap();
-        assert_eq!(from_file_mem_table, mem_table);
-        assert_eq!(from_file_mem_table.data, mem_table.data);
-        assert_eq!(from_file_mem_table.wal, mem_table.wal);
+    #[test]
+    fn test_sst_write() {
+        let mut mem_table = MemTable::new();
+        for i in 0..100 {
+            mem_table.data.push(MemTableEntry {
+                e_type: "PUT".to_string(),
+                key: i.to_string(),
+                value: i+10,
+            });
+        }
+
+        match flush(&mem_table) {
+            Ok(m) => { m },
+            Err(e) => { panic!("{:?}", e); }
+        };
+
+        let f_sst= _load_from_file().unwrap();
+
+        // TODO index
+
+        assert_eq!(f_sst.data_blocks.len(), 100/4);
+        assert_eq!(f_sst.block_size, 4);
+
+        println!("{}", f_sst.data_blocks[0]);
     }
 }
